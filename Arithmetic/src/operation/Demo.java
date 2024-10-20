35fdbde3c7b2e4401e2a7b43fd75f91826e39119
A <A@HACKER-K16G75U>
2024-10-20 13:46:03 +0530

    add statement

--- Arithmetic/src/operation/Demo.java
+++ Arithmetic/src/operation/Demo.java
@@ -7,7 +7,7 @@ public class Demo {
 	int a=10;
 	int b=20;
 
-	
+	System.out.println("select opreation");
 	System.out.println( "Addition of a & b is :" +(a+b));
 	System.out.println("Subtraction of a & b is :" +(b-a));
 	System.out.println("Multiplication of a & b is :" +(a*b));
